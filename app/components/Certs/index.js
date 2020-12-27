b6e94949bc629f957345b77e3bfd3222cc5af9c6
Zack Shone <zackariahshone@gmail.com>
2020-12-27 16:36:50 -0700

    set up

---
+++ app/components/Certs/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Certs
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Certs() {
+  return <div />;
+}
+
+Certs.propTypes = {};
+
+export default Certs;
