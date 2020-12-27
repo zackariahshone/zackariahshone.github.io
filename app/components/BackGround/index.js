c0aa6e6bc67ad3bbd09e5837b459d453a7aa1a88
Zack Shone <zackariahshone@gmail.com>
2020-12-27 16:51:52 -0700

    add components

---
+++ app/components/BackGround/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * BackGround
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function BackGround() {
+  return <div />;
+}
+
+BackGround.propTypes = {};
+
+export default BackGround;
---
+++ app/components/Homesection/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Homesection
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Homesection() {
+  return <div />;
+}
+
+Homesection.propTypes = {};
+
+export default Homesection;
