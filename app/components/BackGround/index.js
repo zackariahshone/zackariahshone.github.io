8b333cd60f6247c071b72b2bc3ed3646ab9c7378
Zack Shone <zackariahshone@gmail.com>
2021-01-01 10:02:41 -0700

    carosel style added

--- app/components/BackGround/index.js
+++ app/components/BackGround/index.js
@@ -19,65 +19,91 @@ function BackGround() {
   return (
     <div className="background" id="section4">
       <Container>
-        <div />
+        <div className="bgTitle">
+          <h1>Background: </h1>
+        </div>
       </Container>
-      <Container>
-        <Row>
-          <Col>
-            <Carousel>
-              <Carousel.Item>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src={img} />
-                </Card>
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src={img} />
-                </Card>
 
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Card style={{ width: '18rem' }}>
+      <Carousel>
+        <Carousel.Item>
+          <Container>
+            <Row>
+              <Col>
+                <Card style={{ width: '30%', margin: '10%' }}>
                   <Card.Img variant="top" src={img} />
                 </Card>
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Card style={{ width: '18rem' }}>
+              </Col>
+              <Col>
+                <h3>Cycling</h3>
+              </Col>
+            </Row>
+          </Container>
+          <Carousel.Caption>
+            <p>Turning the cranks since I was 18.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row>
+              <Col>
+                <Card style={{ width: '30%', margin: '10%' }}>
                   <Card.Img variant="top" src={img} />
                 </Card>
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
+              </Col>
+              <Col>
+                <h3>Bike Shop and turning wrenches</h3>
+              </Col>
+            </Row>
+          </Container>
+          <Carousel.Caption>
+            <p>
+              First job earned at 16 with my work ethic, not getting paid but
+              working hard.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row>
+              <Col>
+                <Card style={{ width: '30%', margin: '10%' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+              </Col>
+              <Col>
+                <h3>Nutrition</h3>
+              </Col>
+            </Row>
+          </Container>
+          <Carousel.Caption>
+            <p>
+              Had a strong interest in nutrition coming out of school but
+              pursued pro cycling instead.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row>
+              <Col>
+                <Card style={{ width: '30%', margin: '10%' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+              </Col>
+              <Col>
+                <h3>Computer Science</h3>
+              </Col>
+            </Row>
+          </Container>
+          <Carousel.Caption>
+            <p>
+              As I was racing i met so many awesome individuals and finally met
+              someone who mentioned computer science and the rest is in this
+              website!
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
