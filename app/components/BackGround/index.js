c29d89bbcc2b94a5ea1da4ca8290059452bef109
Zack Shone <zackariahshone@gmail.com>
2021-01-01 09:36:51 -0700

    carosel added

--- app/components/BackGround/index.js
+++ app/components/BackGround/index.js
@@ -9,6 +9,9 @@ import './style.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import img from '../../images/icon-512x512.png';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
@@ -16,18 +19,62 @@ function BackGround() {
   return (
     <div className="background" id="section4">
       <Container>
-        <div className="bgTitle">
-          <h1>BackGround: </h1>
-        </div>
+        <div />
+      </Container>
+      <Container>
         <Row>
           <Col>
-            <h1>Cycling</h1>
-          </Col>
-          <Col>
-            <h1>Work</h1>
-          </Col>
-          <Col>
-            <h1>Code</h1>
+            <Carousel>
+              <Carousel.Item>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl
+                    consectetur.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={img} />
+                </Card>
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl
+                    consectetur.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </Col>
         </Row>
       </Container>
